ab91a43233aa00e0d7f1eafc260fc29b3774d48a
zhaoruike <1132787601@qq.com>
2018-07-30 16:07:01 +0800

    修改index

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ let session = require('express-session');
 let crypto = require('crypto');
 let config = require("./config.js")
 let request = require("request")
+let xml2js=require('xml2js')
 
 
 let port = process.env.PORT || 80;
@@ -19,6 +20,21 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('dist'));
 
+let url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + config.appID + "&secret=" + config.appSecret
+    request(url, function (error, response, data) {
+        if(data){
+           var newData = JSON.parse(data);
+           var url =  `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${newData.access_token}`
+           request.post({url:url,form:config.menu},function(error1, response1, data1){
+               console.log(data1)
+           })
+        }
+    })
+
+ var rui = 'http://140.143.144.215/wx/response'
+ var code = 'code'
+ var SCOPE = 'snsapi_userinfo'  // 需要用户授权
+ location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${config.appID}&redirect_uri=${rui}&response_type=${code}&scope=${SCOPE}&state=STATE#wechat_redirect`
 
 app.get("/wx", function (req, res) {
     var signature = req.query.signature,
@@ -47,17 +63,58 @@ app.get("/wx", function (req, res) {
 
 })
 
-app.get("/token", function (req, res) {
+app.get("/wx/token", function (req, res) {
     let url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=" + config.appID + "&secret=" + config.appSecret
     request(url, function (error, response, data) {
         if(data){
            var newData = JSON.parse(data);
            var url =  `https://api.weixin.qq.com/cgi-bin/menu/create?access_token=${newData.access_token}`
-           request.post({url:url,form:config.menu,function(error1, response1, data1){
+           request.post({url:url,form:config.menu},function(error1, response1, data1){
                console.log(data1)
-           }})
+           })
         }
     })
+})
+
+app.post('/wx',(req, res)=>{
+    var xml = ''
+    var json = null
+    req.on('data',(chunk)=>{
+        xml += chunk
+    })
+    req.on('end',()=>{
+        //将接受到的xml数据转化为json
+        xml2js.parseString(xml,  {explicitArray : false}, function(err, json) {  
+
+            var backTime = new Date().getTime();  //创建发送时间，整数
+
+            if( json.xml.MsgType == 'event' ){  //消息为事件类型
+
+                if( json.xml.EventKey == 'clickEvent' ){
+                    res.send( getXml( json , backTime , '你戳我干啥...' ) )  //回复用户的消息
+                }
+            }else if( json.xml.MsgType == 'text' ){  //消息为文字类型
+
+                res.send( getXml( json , backTime , `你发"${json.xml.Content}"过来干啥？` ) )  //回复用户的消息
+            }
+
+
+        }); 
+    })
+
+    function getXml( json , backTime , word ){
+        var backXML = `
+                <xml>
+                    <ToUserName><![CDATA[${json.xml.FromUserName}]]></ToUserName>
+                    <FromUserName><![CDATA[${json.xml.ToUserName}]]></FromUserName>
+                    <CreateTime>${backTime}</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[${word}]]></Content>
+                </xml>
+            `
+        return backXML;
+    };
+
 })
 
 http.listen(port, function () {
